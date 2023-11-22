65f508215100ebb7d7c59d7fd1c064fe6ba7c451
ionut <ionut02444@gmail.com>
2023-11-22 11:30:14 +0200

    Project created

---
+++ src/main/java/com/ionut/easydriverentals/EasyDriveRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.ionut.easydriverentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDriveRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDriveRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ionut/easydriverentals/EasyDriveRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionut.easydriverentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDriveRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
